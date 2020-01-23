b79a2e5d55620550452cc2e500421d0fd7bad908
Link Dupont <link@sub-pop.net>
2020-01-23 12:22:37 -0500

    Typo in jq option
    Signed-off-by: Link Dupont <link@sub-pop.net>

--- lsyncd-gen.sh
+++ lsyncd-gen.sh
@@ -13,7 +13,7 @@ EOF
 for NAME in rhel8 rhel7 rhel6; do
 	for ENV in dev test; do
 		HOST="ic-$NAME-$ENV-$HOSTNAME"
-		MAC=$(vm inspect --format json $HOST | jq --raw-outpu .Devices.Interfaces[0].MAC.Address)
+		MAC=$(vm inspect --format json $HOST | jq --raw-output .Devices.Interfaces[0].MAC.Address)
 		IP=$(arp -a | grep $MAC | awk '{print $2}' | sed -s "s/[\(\)]//g")
 		if [ "$IP"  != "" ]; then
 			cat <<EOF >> lsyncd-$HOSTNAME.conf 
